0291c0c5ccb8673aa413fbe482bf11c2b7290133
adimahesh <adimahesh55475@gmail.com>
2024-12-21 16:31:32 +0530

    String added

---
+++ String/Stringspaces.java
@@ -0,0 +1,11 @@
+package String;
+import java.util.*;
+public class Stringspaces {
+    public static void main(String[] args) {
+    String str = "   Hello World";
+    String x =  str.trim();
+    System.out.println(x);
+}
+}
+
+
